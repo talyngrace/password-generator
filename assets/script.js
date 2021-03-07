39eea3ae34f502fca273ffa225ac1f20ba9350d7
Talyn Demi <talyndemi@Talyns-MacBook-Air.local>
2021-03-07 09:41:23 -0500

    Added functions for all the random letters, numbers, and symbols

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Get random lowercase letter function
+
+function randomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+//random upper case letter function
+
+function randomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+//random number function
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+//random symbol
+
+function randomSymbol() {
+  const symbols = '!@#$%^&*(){}.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+//return
+
+console.log(randomNumber());
+console.log(randomLower());
+console.log(randomUpper());
+console.log(randomUpper());
+console.log(randomSymbol());

55dd877d1a0e8b38e0293c909ceaf60022859943
Talyn Demi <talyndemi@Talyns-MacBook-Air.local>
2021-03-08 16:23:54 -0500

    added prompts

--- assets/script.js
+++ assets/script.js
@@ -14,15 +14,6 @@ function writePassword() {
  generateBtn.addEventListener("click", writePassword);
 
 
-//object of all functions
-
- const randomFunction = {
-  lower: randomLower,
-  upper: randomUpper,
-  number: randomNumber,
-  symbol: randomSymbol
-};
-
 //Get random lowercase letter function
 
 function randomLower(){
@@ -48,10 +39,29 @@ function randomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+console.log(randomNumber());
 
-function generatePassword(randomLower, randomUpper, randomNumber, randomSymbol) {
-  return randomLower + randomUpper + randomNumber + randomSymbol;
-};
 
-console.log(generatePassword());
-console.log(randomFunction());
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+
+var randomPassword = prompt("How long would you like your password to be? Between 8-128");
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+randomPassword >= 8 && randomPassword <= 128
+
+
+
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page

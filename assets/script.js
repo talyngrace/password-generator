a7bb183daff21b42da7cc5c849ed41bf3e7ab279
Talyn Demi <talyndemi@Talyns-MacBook-Air.local>
2021-03-09 09:54:20 -0500

    Finished it upgit add .

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var symboles = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "{", "|", "}", "~"]
+var symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "{", "|", "}", "~"]
 
 // // Assignment Code
 
@@ -13,6 +13,10 @@ function generatePassword() {
     prompt
       ('whats the length of the pw')
   );
+  if (isNaN(passLength) === true) {
+    alert("Password length needs to be a number.")
+    return;
+  }
   if (passLength < 8) {
     alert("the password must be at least 8 characters");
     return;
@@ -33,6 +37,8 @@ function generatePassword() {
   var randomSymbol = confirm(
     ("Do you want to use symbols?")
   )
+
+  // console.log(randomLower, randomUpper, randomNumber, randomSymbol);
 // if statement
 
 if (
@@ -44,7 +50,33 @@ if (
   alert('you must pick one character type');
   return;
 }
+var allChar = []
+if (randomUpper === true){
+ allChar = allChar.concat(upperCase)
+  }
+if (randomLower === true){
+ allChar = allChar.concat(lowerCase);
 }
+if (randomNumber === true){
+  allChar = allChar.concat(numbers);
+}
+if (randomSymbol === true){
+  allChar = allChar.concat(symbols);
+ }
+var password = []
+console.log(typeof passLength)
+for (var i = 0; i < passLength; i++){
+  var randomIndex = Math.floor(Math.random() * allChar.length)
+  password.push(allChar[randomIndex])
+  console.log(i);
+} 
+console.log(password);
+return password.join('');
+};
+
+
+
+
 
 // function createPassword() {
 //   var potentialPassword = []
